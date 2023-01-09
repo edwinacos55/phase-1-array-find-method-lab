110194c3570cd707fc3d6ffad9de4317f00676e5
edwinacos55 <edwinacos55@gmail.com>
2023-01-08 19:16:15 -0500

    commit tree c40b6addcfb70a61c4b5084392e181aa57a25490

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin (records) { 
+    const result = (records.find(winning => winning.result=== "W"))
+     return (result ? result.year : result)
+// return records.find(winning => winning.result=== "W")
+}
+
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+superbowlWin(sadReality);
+   
